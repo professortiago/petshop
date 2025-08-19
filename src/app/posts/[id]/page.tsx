c08d0b2912cf04052a9156fc99549d3dd7402360
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-08-19 11:19:40 -0300

    Remove comentários

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,25 +1,19 @@
+// src/app/posts/[id]/page.tsx
 import Container from "@/components/Container";
 import estilos from "./detalhe-post.module.css";
 import { Post } from "@/types/Post";
 import { notFound } from "next/navigation";
 
-// src/app/posts/[id]/page.tsx
 type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
-/* A função abaixo precisa:
-- Receber o id como string
-- Executar o acesso à API usando este id e retornar o post com os dados 
-- O retorno da função DEVE SER uma Promise
-- Não se esqueça de chamar/usar esta nova função dentro do generateMetadata e do DetalhePost no lugar do código que vc irá remover. */
 async function buscarPostPorId(id: string): Promise<Post> {
   const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
     next: { revalidate: 0 },
   });
 
   if (resposta.status === 404) {
-    // Buscar a page not-found.tsx automaticamente em caso de erro 404
     notFound();
   }
 
