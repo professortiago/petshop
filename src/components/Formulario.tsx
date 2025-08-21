2470ccc99ecba3bb081a2bafc5e554ea6b903e7f
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-08-21 11:06:55 -0300

    Gerencia states do Form e do Botão
    Captura dados, envia para o Supabase, gerencia states para exibição de texto no botão.

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,8 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import estilos from "./Formulario.module.css";
+import { enviarContato } from "@/lib/enviar-contato";
+import { useFormStatus } from "react-dom";
+
+function BotaoEnviar() {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? "Processando..." : "Enviar"}
+    </button>
+  );
+}
 
 export default function Formulario() {
+  // Estados para os textos da mensagens
+  const [mensagem, setMensagem] = useState("");
+
+  // Estados para o tipo de mensagem: sucesso ou erro
+  const [tipoMensagem, setTipoMensagem] = useState<"sucesso" | "erro" | "">("");
+
+  async function processarDados(dadosForm: FormData) {
+    // Reset dos states voltando ao valor padrão
+    setMensagem("");
+    setTipoMensagem("");
+
+    try {
+      await enviarContato(dadosForm);
+      setMensagem("Mensagem enviado com sucesso!");
+      setTipoMensagem("sucesso");
+
+      // Reseta os campos do formulário
+      dadosForm.set("nome", "");
+      dadosForm.set("email", "");
+      dadosForm.set("mensagem", "");
+    } catch (error: unknown) {
+      // Verificando se é um erro do type Error, para evitar erros no deploy.
+      setMensagem(error instanceof Error ? error.message : "Erro ao enviar");
+      setTipoMensagem("erro");
+    }
+  }
+
   return (
-    <form action="" className={estilos.formulario}>
+    <form action={processarDados} className={estilos.formulario}>
       <div className={estilos.campo}>
         <label htmlFor="nome">Nome</label>
         <input
@@ -31,7 +72,7 @@ export default function Formulario() {
         ></textarea>
       </div>
       <div className={estilos.campo}>
-        <button type="submit">Enviar</button>
+        <BotaoEnviar />
       </div>
     </form>
   );
